9f9146057f8a5229463851230c75df3f66739a95
Elitsa Georgeva <Elitsa Georgeva@DESKTOP-8D1ST4F>
2023-10-30 14:48:41 +0200

    Add project files.

---
+++ ObjectsAndClassesTask2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Car
+{
+    public string Model;
+    public int Year;
+    public bool IsEngineRunning;
+
+    public Car(string model, int year)
+    {
+        Model = model;
+        Year = year;
+        IsEngineRunning = false;
+    }
+
+    public void StartEngine()
+    {
+        if (!IsEngineRunning)
+        {
+            IsEngineRunning = true;
+            Console.WriteLine($"{Model} ({Year}) engine started.");
+        }
+        else
+        {
+            Console.WriteLine($"{Model} ({Year}) engine has already been started.");
+        }
+    }
+
+    public void StopEngine()
+    {
+        if (IsEngineRunning)
+        {
+            IsEngineRunning = false;
+            Console.WriteLine($"{Model} ({Year}) engine stopped.");
+        }
+        else
+        {
+            Console.WriteLine($"{Model} ({Year}) engine is already stopped.");
+        }
+    }
+}
